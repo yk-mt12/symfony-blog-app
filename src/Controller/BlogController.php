b73ce6f816b2c1b90f8be243f653acb73deebbee
yk-mt12 <agvksl543@gmail.com>
2022-10-20 21:42:39 +0900

    Environment construction completed

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends Controller
+{
+    /**
+     * @Route("/blog", name="blog")
+     */
+    public function index(): Response
+    {
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
+    }
+}
